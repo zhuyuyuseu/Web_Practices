072c747412c3ae1de4a7c7338975880f21f8d243
Yuyu Zhu <zhuyuyuseu@gmail.com>
2015-05-06 23:37:25 +0800

    add pattern

--- node/modules/npm/module_examples/bundle-parser.js
+++ node/modules/npm/module_examples/bundle-parser.js
@@ -1,7 +1,7 @@
 //import a built-in module
 var fs = require('fs');
 var Bundle = require('./lib/bundle.js');
-var utils = require('vanguard-utils')();
+//var utils = require('vanguard-utils')();
 
 // var bundle = new Bundle('bundle',[]);
 // console.log(bundle.title);
@@ -14,9 +14,15 @@ fs.readFile('./UDSDocuments.html', options, function(err, data){
   if(err){
     console.log("Read failed: %s", err.code);
   }else{
-    var bundles = [], matchResult = null, regexSectionPattern = /<class\="title">\s*<h2>/gm;
+    var bundles = [], matchResult = null, regexSectionPattern = /<div\s+class\="title">[\w\W]*?<\/table>/gm,
+        bundleSection = null;
     while((matchResult = regexSectionPattern.exec(data)) !== null){
-      console.log(matchResult[0]);
+      bundleSection = matchResult[0];
+      if(bundleSection){
+        var firstIndex = bundleSection.indexOf('<h2>'), endIndex = bundleSection.indexOf('</h2>');
+        console.log(bundleSection.substr(firstIndex + 4, endIndex - firstIndex - 4));
+        break;
+      }
     }
     // console.log(data);
   }
