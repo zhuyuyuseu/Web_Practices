5dc69bbd9a7427abeba33ddbe923e40b11c6dfea
John Riewerts <John.Riewerts@acxiom.com>
2016-09-20 16:36:52 -0500

    remove the repo please

--- node/testing/examples/test/test_expiration.js
+++
@@ -1,9 +0,0 @@
-describe('test aync with time out', function(){
-  this.timeout(500);
-
-  it('time out call', function(){
-    setTimeout(function(){
-      console.log("time out");
-    }, 1000);
-  });
-});
